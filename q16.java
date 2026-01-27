6ef70b93b098337c2e54d39604c5ef3b8afc6893
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-01-27 23:19:52 +0530

    q16

---
+++ q16.java
@@ -0,0 +1,23 @@
+class Main{
+  public static void main(String[] args) {
+    int arr[]={4,1,2,1,2};
+    HashMap<Integer,Integer>result=new HashMap<>();
+    for(int i=0;i<=arr.length-1;i++){
+      if(result.containsKey(arr[i])){
+        result.put(arr[i],result.get(arr[i])+1);
+      }
+      else{
+        result.put(arr[i],1);
+      }
+    }
+   
+   
+    for(int key:result.keySet()){
+      if(result.get(key)==1){
+        System.out.println(key);
+        break;
+      }
+     
+    }
+  }
+}
